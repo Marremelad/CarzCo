792048c166145c6ef091949e54265ee70206f0f0
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-08 11:22:28 +0200

    Implement Motorcycle class

--- Motorcycle.cs
+++ Motorcycle.cs
@@ -1,6 +1,12 @@
 ﻿namespace CarzCo;
 
-public class Motorcycle
+public class Motorcycle : Vehicle
 {
-    
+    public bool HasSideCart { get; set; }
+
+    public Motorcycle(string model, string make, bool hasSideCart) :
+        base(make, model)
+    {
+        HasSideCart = hasSideCart;
+    }
 }
