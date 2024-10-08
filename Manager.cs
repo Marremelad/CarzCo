62d0c10adaa8a00da1744af91b7eccc2c9455488
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-08 13:29:29 +0200

    Implement Manager class

--- Manager.cs
+++ Manager.cs
@@ -1,6 +1,28 @@
 ﻿namespace CarzCo;
 
-public class Manager
+public abstract class Manager
 {
+    private static List<Vehicle> _vehicles = new List<Vehicle>();
+
+
+    public static void AddVehicle(Vehicle vehicle)
+    {
+        _vehicles.Add(vehicle);
+    }
+
+    public static void AddVehicle(IList<Vehicle> vehicles)
+    {
+        foreach (var vehicle in vehicles)
+        {
+            _vehicles.Add(vehicle);
+        }
+    }
     
+    public static void DisplayVehicles()
+    {
+        foreach (var vehicle in _vehicles)
+        {
+            Console.WriteLine($"{vehicle.Make} {vehicle.Model} - Fuel Type: {vehicle.FuelType}, Max Velocity: {vehicle.MaxSpeed} km/h");
+        }
+    }
 }
