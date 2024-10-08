cdaa1c0974634cca77d1fd0fd21becd0ae36d3d7
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-08 11:32:29 +0200

    Implement Truck class

--- Truck.cs
+++ Truck.cs
@@ -1,6 +1,12 @@
 ﻿namespace CarzCo;
 
-public class Truck
+public class Truck : Vehicle
 {
-    
+    public int NumberOfAxels { get; set; }
+
+    public Truck(string make, string model, int numberOfAxels) :
+        base(make, model)
+    {
+        NumberOfAxels = numberOfAxels;
+    }
 }
