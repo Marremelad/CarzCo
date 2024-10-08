a48d616b60deafc5a885b27eca173eb166f63866
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-08 11:31:03 +0200

    Change order of make and model

--- Motorcycle.cs
+++ Motorcycle.cs
@@ -4,7 +4,7 @@ public class Motorcycle : Vehicle
 {
     public bool HasSideCart { get; set; }
 
-    public Motorcycle(string model, string make, bool hasSideCart) :
+    public Motorcycle(string make, string model, bool hasSideCart) :
         base(make, model)
     {
         HasSideCart = hasSideCart;
