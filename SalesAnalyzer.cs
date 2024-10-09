7c43beaac341ac245dcb4a466004a85c9463f14b
Anton-Dahlstrom <anton.dahlstrom@hotmail.com>
2024-10-09 13:25:56 +0200

    add sales analyzer with average and total

---
+++ SalesAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿namespace CarzCo
+{
+    internal class SalesAnalyzer
+    {
+        protected List<SaleRecord> FilterVehicleType<T>(List<SaleRecord> records) where T : Vehicle
+        {
+            if (typeof(T) != typeof(Vehicle))
+            {
+                records = records.Where(v => v.Vehicle.GetType() == typeof(T)).ToList();
+            }
+            return records;
+            
+        }
+        public double TotalRevenue<T>(List<SaleRecord> records) where T : Vehicle
+        {
+            records = FilterVehicleType<T>(records);
+            return records.Sum(r => r.SellPrice);
+        }
+        public double AveragePrice<T>(List<SaleRecord> records) where T : Vehicle
+        {   
+            records = FilterVehicleType<T>(records);
+            return records.Average(r => r.SellPrice);
+        }
+    }
+}
