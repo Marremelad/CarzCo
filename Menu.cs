bf2eef6f00fb340b3d999daee14e0e7555fe9eb9
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-09 15:04:38 +0200

    Start implementing DisplayMenu method

--- Menu.cs
+++ Menu.cs
@@ -1,6 +1,18 @@
-﻿namespace CarzCo;
+﻿using Spectre.Console;
+namespace CarzCo;
 
 public class Menu
 {
+    public void DisplayMenu()
+    {
+        var fruit = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("What's your [green]favorite fruit[/]?")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                .AddChoices(new[] {
+                    "Vehicles", "Maintenance Record", "Reserved Vehicles",
+                }));
+    }
     
 }
