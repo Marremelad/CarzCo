108e806c5d048b36153165bc0767cf3cbd9132d1
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-09 15:56:08 +0200

    Change test code to test Menu class

--- Program.cs
+++ Program.cs
@@ -1,23 +1,42 @@
-﻿
+﻿using Spectre.Console;
+
 namespace CarzCo;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Ask for the user's favorite fruit
-        var fruit = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title("What's your [green]favorite fruit[/]?")
-                .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
-                .AddChoices(new[] {
-                    "Apple", "Apricot", "Avocado", 
-                    "Banana", "Blackcurrant", "Blueberry",
-                    "Cherry", "Cloudberry", "Cocunut",
-                }));
+        List<Vehicle> vehiclesList =
+        [
+            new Car("Honda", "Civic", 4, FuelType.Petrol, 500),
+            new Car("Toyota", "Yaris", 4, FuelType.Petrol, 500),
+            new Car("Hyundai", "I30 N", 4, FuelType.Petrol, 500),
 
-// Echo the fruit back to the terminal
-        AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
+            new Motorcycle("Kawasaki", "Ninja", false, FuelType.Petrol, 300),
+            new Motorcycle("BMW", "S1000RR", false, FuelType.Petrol, 300),
+            new Motorcycle("Yamaha", "R9", false, FuelType.Petrol, 300),
+
+            new Truck("Ford", "Raptor", 4, FuelType.Diesel, 200),
+            new Truck("Dodge", "Ram", 6, FuelType.Diesel, 200),
+            new Truck("Toyota", "Hilux", 8, FuelType.Diesel, 200),
+
+            new Boat("Big", "Boat", 100, FuelType.Diesel, 50),
+            new Boat("Medium", "Boat", 50, FuelType.Diesel, 50),
+            new Boat("Small", "Boat", 10, FuelType.Diesel, 50)
+        ];
+        
+        List<Vehicle> vehiclesToReserve =
+        [
+            vehiclesList[0],
+            vehiclesList[3],
+            vehiclesList[6],
+            vehiclesList[9]
+
+        ];
+        
+        Manager.AddVehicle(vehiclesList);
+        Manager.ReserveVehicle(vehiclesToReserve);
+        
+        Menu.MainMenu();
     }
 }
