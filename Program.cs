f372acf2b17f4b8f368512539ce8f730a665ea53
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-09 12:26:36 +0200

    Change test code

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Car car = new Car("Toyota", "Supra", 4, FuelType.Bensin, 500);
+        /*Car car = new Car("Toyota", "Supra", 4, FuelType.Petrol, 500);
 
         MaintenanceRecord service = new MaintenanceRecord(car.VehicleId, ServiceType.Oil);
         
         Manager.AddService(service);
-        Manager.DisplayService(car.VehicleId);
+        Manager.DisplayService(car.VehicleId);*/
+        
+        Manager.DisplayVehicles();
     }
 }
