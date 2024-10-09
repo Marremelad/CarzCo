2c9b837c308a8a04cae7035a74dbc7e9706cad2d
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-09 15:56:56 +0200

    Implement VehiclesMenu and ReservedVehiclesMenu methods

--- Menu.cs
+++ Menu.cs
@@ -3,16 +3,45 @@ namespace CarzCo;
 
 public class Menu
 {
-    public void DisplayMenu()
+    public static void MainMenu()
     {
-        var fruit = AnsiConsole.Prompt(
+        var choice = AnsiConsole.Prompt(
             new SelectionPrompt<string>()
-                .Title("What's your [green]favorite fruit[/]?")
+                .Title("Welcome to Carz Co")
                 .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
                 .AddChoices(new[] {
-                    "Vehicles", "Maintenance Record", "Reserved Vehicles",
+                    "Vehicles", "Reserved Vehicles",
                 }));
+        
+        switch (choice)
+        {
+            case "Vehicles":
+                VehiclesMenu(Manager.GetVehicles());
+                break;
+            case "Reserved Vehicles":
+                ReservedVehiclesMenu(Manager.GetReservedVehicles());
+                break;
+        }
     }
-    
+
+    private static void VehiclesMenu(List<Vehicle> vehicles)
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<Vehicle>()
+                .Title("Vehicles in stock")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more vehicles)[/]")
+                .AddChoices(vehicles));
+    }
+
+    private static void ReservedVehiclesMenu(Dictionary<int, Vehicle> reservedVehicles)
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<Vehicle>()
+                .Title("Reserved vehicles")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more vehicles)[/]")
+                .AddChoices(reservedVehicles.Values));
+    }
 }
