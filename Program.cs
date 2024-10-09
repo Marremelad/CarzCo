7059f8523d4c495a5dfc150a42d0e5b5beb58226
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-09 14:50:31 +0200

    Remove code

--- Program.cs
+++ Program.cs
@@ -1,47 +1,23 @@
-﻿namespace CarzCo;
+﻿
+namespace CarzCo;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<Vehicle> vehiclesList = new List<Vehicle>()
-        {
-            new Car("Honda", "Civic", 4, FuelType.Petrol, 500),
-            new Car("Toyota", "Yaris", 4, FuelType.Petrol, 500),
-            new Car("Hyundai", "I30 N", 4, FuelType.Petrol, 500),
+        // Ask for the user's favorite fruit
+        var fruit = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("What's your [green]favorite fruit[/]?")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                .AddChoices(new[] {
+                    "Apple", "Apricot", "Avocado", 
+                    "Banana", "Blackcurrant", "Blueberry",
+                    "Cherry", "Cloudberry", "Cocunut",
+                }));
 
-            new Motorcycle("Kawasaki", "Ninja", false, FuelType.Petrol, 300),
-            new Motorcycle("BMW", "S1000RR", false, FuelType.Petrol, 300),
-            new Motorcycle("Yamaha", "R9", false, FuelType.Petrol, 300),
-
-            new Truck("Ford", "Raptor", 4, FuelType.Diesel, 200),
-            new Truck("Dodge", "Ram", 6, FuelType.Diesel, 200),
-            new Truck("Toyota", "Hilux", 8, FuelType.Diesel, 200),
-
-            new Boat("Big", "Boat", 100, FuelType.Diesel, 50),
-            new Boat("Medium", "Boat", 50, FuelType.Diesel, 50),
-            new Boat("Small", "Boat", 10, FuelType.Diesel, 50)
-        };
-
-        Car carToSell = new Car("Test", "Sell", 4, FuelType.Ethanol, 500);
-        Motorcycle MotoToSell = new Motorcycle("Test2", "Sell2", false, FuelType.Diesel, 500);
-
-        Dictionary<int, Vehicle> reservedVehicles = new Dictionary<int, Vehicle>()
-        {
-            { vehiclesList[0].VehicleId, vehiclesList[0] },
-            { vehiclesList[3].VehicleId, vehiclesList[3] },
-            { vehiclesList[6].VehicleId, vehiclesList[6] },
-            { vehiclesList[9].VehicleId, vehiclesList[9] },
-        };
-        
-        Manager.AddVehicle(carToSell);
-        Manager.AddVehicle(MotoToSell);
-        
-        Manager.SellVehicle(carToSell, 500);
-        Manager.SellVehicle(MotoToSell, 5000);
-
-        Console.WriteLine(SalesAnalyzer<Motorcycle>.AveragePrice(Manager._SaleRecords));
-        Console.WriteLine(SalesAnalyzer<Car>.TotalRevenue(Manager._SaleRecords));
-
+// Echo the fruit back to the terminal
+        AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
     }
 }
