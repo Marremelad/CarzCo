127c055ef165ac3bbcb72e66d2de6b1286015308
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-09 17:04:16 +0200

    Implement VehicleOptionsMenu and create method to check if choice is a string

--- Menu.cs
+++ Menu.cs
@@ -5,9 +5,10 @@ public class Menu
 {
     public static void MainMenu()
     {
+        Console.Clear();
         var choice = AnsiConsole.Prompt(
             new SelectionPrompt<string>()
-                .Title("Welcome to Carz Co")
+                .Title("\n  Welcome to Carz Co. The leading console app for buying cars!")
                 .PageSize(10)
                 .MoreChoicesText("[grey](Move up and down to reveal more options)[/]")
                 .AddChoices(new[] {
@@ -26,43 +27,70 @@ public class Menu
         }
     }
 
-    private static void VehiclesMenu(List<Vehicle> vehicles)
+    private static bool CheckChoice(object choice)
     {
-        var choice = AnsiConsole.Prompt(
-            new SelectionPrompt<object>()
-                .Title("Vehicles in stock")
-                .PageSize(10)
-                .MoreChoicesText("[grey](Move up and down to reveal more vehicles)[/]")
-                .AddChoices(vehicles)
-                .AddChoices("Return to main menu"));
-
         if (choice is string stringValue)
         {
             if (stringValue == "Return to main menu")
             {
-                MainMenu();
+                return true;
             }
         }
-        else Console.WriteLine("Hello, World!");
+        return false;
+    }
+
+    private static void VehiclesMenu(List<Vehicle> vehicles)
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<object>()
+                .Title("\n  Vehicles in stock")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more vehicles)[/]")
+                .AddChoices(vehicles)
+                .AddChoices("Return to main menu"));
+        
+        if (CheckChoice(choice)) MainMenu();
+        else VehicleOptionsMenu((Vehicle)choice);
     }
 
     private static void ReservedVehiclesMenu(Dictionary<int, Vehicle> reservedVehicles)
     {
         var choice = AnsiConsole.Prompt(
             new SelectionPrompt<object>()
-                .Title("Reserved vehicles")
+                .Title("\n  Reserved vehicles")
                 .PageSize(10)
                 .MoreChoicesText("[grey](Move up and down to reveal more vehicles)[/]")
                 .AddChoices(reservedVehicles.Values)
                 .AddChoices("Return to main menu"));
         
-        if (choice is string stringValue)
+        if (CheckChoice(choice)) MainMenu();
+    }
+
+    private static void VehicleOptionsMenu(Vehicle vehicle)
+    {
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title($"\n  {vehicle.Make} {vehicle.Model}")
+                .PageSize(5)
+                .MoreChoicesText("[grey](Move up and down to reveal more vehicles)[/]")
+                .AddChoices(new[] {
+                    "Buy this vehicle",
+                    "Reserve this vehicle",
+                }));
+        if (CheckChoice(choice)) MainMenu();
+        
+        switch (choice)
         {
-            if (stringValue == "Return to main menu")
-            {
-                MainMenu();
-            }
+            case "Buy this vehicle":
+                Manager.SellVehicle(vehicle, 500);
+                Console.WriteLine("Thank you for visiting Carz Co. Have a nice ride.");
+                break;
+            case "Reserve this vehicle":
+                Manager.ReserveVehicle(vehicle);
+                Console.WriteLine("Your vehicle has been reserved.");
+                break;
         }
-        else Console.WriteLine("Hello, World!");
+        Thread.Sleep(3000);
+        MainMenu();
     }
 }
