c99a20705f046b9c5ad299ab202ca84e706c37ad
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-10 12:36:16 +0200

    Add comments to MaintenanceRecord.cs

--- MaintenanceRecord.cs
+++ MaintenanceRecord.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarzCo
+﻿namespace CarzCo
 {
+    // Class representing a maintenance record for a vehicle.
     public class MaintenanceRecord
     {
+        // Unique identifier for the maintenance record.
         public readonly int Id;
+
+        // Type of service performed (e.g., repair, inspection).
         public ServiceType ServiceType { get; set; }
+
+        // Date the service was performed, defaulting to today's date.
         public DateTime Date = DateTime.Today;
-        internal MaintenanceRecord(int id, ServiceType servicetype) {
+
+        // Constructor for initializing a maintenance record with an ID and service type.
+        internal MaintenanceRecord(int id, ServiceType servicetype) 
+        {
             Id = id;
             ServiceType = servicetype;
         }    
